b7589bd156bc90f3c92983181232065c9e5e6f4f
alscjf909 <alscjf909@naver.com>
2021-03-16 00:59:55 +0900

    210316

---
+++ opencv/project2.py
@@ -0,0 +1,86 @@
+import cv2 
+import numpy as np
+
+
+widthImg = 640
+heightImg = 480
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
+    imgCanny = cv2.Canny(imgBlur, 200, 200)
+    kernel = np.ones((5,5))
+    imgDial = cv2.dilate(imgCanny, kernel, iterations=2)
+    imgThres = cv2.erode(imgDial, kernel, iterations=1)
+
+    return imgThres
+
+
+def getContours(img): # 윤곽띠
+    biggest = np.array([])
+    maxArea = 0
+    contours, hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area>5000:
+            peri = cv2.arcLength(cnt, True) #호 길이 보여줌
+            #print(peri)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri, True) #호의 길이를 측정하기 위해 찍힌 점)
+            if area > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    cv2.drawContours(imgContour,cnt,-1,(255,255,0),3)
+    return biggest
+
+
+
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3,widthImg)
+cap.set(4,heightImg)
+cap.set(10,150) # brightness
+
+while True:
+    succes, img=cap.read()
+    cv2.resize(img,(widthImg,heightImg))
+    imgContour = img.copy()
+    imgThres = preProcessing(img)
+    getContours(imgThres)
+    cv2.imshow("Result",imgContour)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+
+
